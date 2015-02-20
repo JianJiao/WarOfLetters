15f69e1ceacb88b304982f09749e2347b7fa82ea
JianJiao <jianj@andrew.cmu.edu>
2015-02-19 20:14:59 -0800

    add missile and create the class

---
+++ WarOfLetters.spritebuilder/Source/Missile.h
@@ -0,0 +1,13 @@
+//
+//  Missile.h
+//  WarOfLetters
+//
+//  Created by Jian Jiao on 2/19/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Missile : CCSprite
+
+@end
---
+++ WarOfLetters.spritebuilder/Source/Missile.m
@@ -0,0 +1,13 @@
+//
+//  Missile.m
+//  WarOfLetters
+//
+//  Created by Jian Jiao on 2/19/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Missile.h"
+
+@implementation Missile
+
+@end
